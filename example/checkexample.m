f9b8d5fb1e835774f5c3982830e0b870f33c1713
Zuben Scott <27812066+zubenscott@users.noreply.github.com>
2024-08-22 11:58:35 -0700

    code to extract network object and polygon information

--- example/checkexample.m
+++ example/checkexample.m
@@ -1,6 +1,14 @@
 % watch movie of output from example1
+% then extract a network object and polygon information
+%
+% requires networktools code from : 
+% https://github.com/lenafabr/networktools
+%
+% and spatialgraph2D code from FileExchange:
+% https://www.mathworks.com/matlabcentral/fileexchange/73630-spatialgraph2d
 
 addpath('../../networktools/')
+addpath('../../spatialgraph2D/')
 
 %% read and visualize from file with many snapshots
 data = dlmread('example1.dump.out','',0,1);
@@ -14,3 +22,29 @@ options.step=20;
 options.dt=1e-3*10; % timestep times snapshot frequency
 options.axis=false;
 viewSnapshots2(xs,ys,es,ecs,options)
+
+%% Extract network and polygons
+
+% which frame:
+tc=10;
+
+% make network
+NT = NetworkObj;
+% set node positions and connections
+NT.nodepos = [xs(tc,:)' ys(tc,:)'];
+for ec = 1:ecs(tc)
+    NT.edgenodes(ec,:) = [es{ec}(tc,1) es{ec}(tc,2)];
+end
+
+% setup network, reset edgelengths, remove double edges and only
+% keeplargest component in case disconnected
+NT.setupNetwork(true);
+NT.removeDoubleEdges;
+NT.keepLargestConnComp;
+
+%% make graph, used for spatialgraph2D
+G = NT.makeGraph;
+% spatial graph 2d object:
+obj = spatialgraph2D(G,NT.nodepos(:,1)',NT.nodepos(:,2)');
+% obtain polyshape array of all constituent polygons:
+pgon = polyshape(obj);
